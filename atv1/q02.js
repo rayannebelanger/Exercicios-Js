918f47aceb3b9c1a9121789204297b767bb1d13f
Rayanne Belanger <135059362+rayannebelanger@users.noreply.github.com>
2024-03-30 23:10:32 -0300

    atv1

---
+++ atv1/q02.js
@@ -0,0 +1,5 @@
+v1 = Number (prompt("Insira o primeiro número"))
+v2 = Number (prompt("Insira o segundo número"))
+v3 = Number (prompt("Insira o terceiro número"))
+
+alert(`Média: ${(v1 + v2 + v3) / 3}`)
---
+++ atv1/q03.js
@@ -0,0 +1,5 @@
+v1 = Number (prompt("Insira o primeiro número"))
+v2 = Number (prompt("Insira o segundo número"))
+v3 = Number (prompt("Insira o terceiro número"))
+
+document.write(`${v1}, ${v2}, ${v3}`)
